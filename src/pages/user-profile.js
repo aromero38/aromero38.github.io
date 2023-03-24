b91a635b5397d33b65d2d5de50c5f68f82c1b909
aromero38 <71117150+aromero38@users.noreply.github.com>
2023-03-24 00:42:25 -0500

    Added test music player template

--- src/pages/user-profile.js
+++ src/pages/user-profile.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
 import {getProviders, signOut} from "next-auth/react"
-import spotifyApi from 'lib/spotify'
 import {useSession} from 'next-auth/react'
 import { useEffect } from 'react'
 import SpotifyWebApi from 'spotify-web-api-node'
+import spotifyApi from 'lib/spotify'
 import { currentlyPlayingSong } from "../../lib/spotify";
 
 
 export default function UserProfile({providers}) {
   const {data: session} = useSession();
+
+  let currSong = spotifyApi.getMyCurrentPlayingTrack();
+
 
   return (
     <>
@@ -21,24 +24,38 @@ export default function UserProfile({providers}) {
 
       {/* nav stuff */}
       <h1 className='text-white p-5 pb-24 font-bold '>LOGO | TUNEFY</h1>
-      <div className='text-white p-5 pb-24 font-bold '>
+      <div className='text-white p-5 pb-24 font-bold'>
         <button onClick={() => signOut({ callbackUrl: 'http://localhost:3000/' })}>sign out</button>
       </div>
 
       {/* heading stuff - username, image, etc.*/}
       <div className='text-white pl-5 flex flex-row w-full place-items-center'>
-        <img className='h-48 w-48 mr-12 rounded-full' src={session?.user.image} />
+        <img className='h-24 w-24 mr-12 rounded-full' src={session?.user.image} />
         <p className='text-2xl'>Hello, {session?.user.name}</p>
       </div>
 
       {/* actual content */}
-      <div>
+      <div className='text-white'>
+      hi {currSong.string}
+       
 
       </div>
 
-      {/* music player */}
+      {/* temp music player */}
       <div>
+        <center>
+          <button className='text-white'>
+            &lt; &lt;
+          </button>
 
+          <button className='text-white'>
+            <img className='h-12 w-12' src='https://www.freepnglogos.com/uploads/play-button-png/index-media-cover-art-play-button-overlay-5.png'/>
+          </button>
+
+          <button className='text-white'>
+            &gt; &gt;
+          </button>
+        </center>
       </div>
     </>
   );
