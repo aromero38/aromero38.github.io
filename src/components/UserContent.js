f32b81b24afb7fc8378858b9f1c4584d39e89521
Alejandro Romero-Lopez <aromero012101@gmail.com>
2023-11-12 13:02:42 -0600

    Added a hook to save user stats to db

--- src/components/UserContent.js
+++ src/components/UserContent.js
@@ -1,28 +1,52 @@
 import { useSession} from "next-auth/react";
 import { useEffect, useState } from 'react';
+import { sql } from '@vercel/postgres';
+import { useRouter } from 'next/router';
 
 import useSpotify from 'hooks/useSpotify.js';
-//import useTopArtists from 'hooks/useTopArtists.js';
 
 const UserContent = () => {
+    const router = useRouter();
+    const user_id = router.query.user === undefined ? '' : (router.query.user).toString();
+    console.log("user_ID: " + user_id)
+
     const spotifyApi = useSpotify();
     const {data: session} = useSession();
-	const [myTopArtists, setMyTopArtists] = useState([])
-    const [myTopSongs, setmyTopSongs] = useState([])
+	const [myTopArtists, setMyTopArtists] = useState(null)
+    const [myTopSongs, setmyTopSongs] = useState(null)
 
-    spotifyApi.getMyTopTracks()
-    .then(
-        function(data) {
-            let topTracks = data.body.items;
-            console.log(topTracks);
-        }, 
-        function(err) {
-            console.log('Something went wrong!', err);
+    const fetchData = async () => {
+        try {
+        if(user_id === '' && myTopArtists === null & myTopSongs === null){
+          fetchTop();
+          const requestBody = {
+            user_email: session.user.email,
+            top_artists: myTopArtists,
+            top_songs: myTopSongs,
+          };
+          const response = await fetch(`/api/setStats`, {
+            method: 'POST', // Use the POST method to send data in the body
+            headers: {
+              'Content-Type': 'application/json', // Specify the content type as JSON
+            },
+            body: JSON.stringify(requestBody), // Convert the JavaScript object to JSON
+          });
+          const fetchedData = await response.json();
+          console.log(fetchedData);
         }
-    );
+        else if (user_id !== '' && myTopArtists === null && myTopSongs === null){
+            const response = await fetch(`/api/getStats?user_id=${user_id}`);
+            const fetchedData = await response.json();
+            console.log(fetchedData);
+        }
 
-    const fetchTop = () => {
-            spotifyApi.getMyTopArtists()
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+
+    const fetchTop = async () => {
+            await spotifyApi.getMyTopArtists()
             .then(
                 function(data) {
                     setMyTopArtists(data.body.items);
@@ -32,7 +56,7 @@ const UserContent = () => {
                 }
             );
 
-            spotifyApi.getMyTopTracks()
+            await spotifyApi.getMyTopTracks()
             .then(
                 function(data) {
                     setmyTopSongs(data.body.items);
@@ -95,7 +119,7 @@ const UserContent = () => {
 
     useEffect(() => {
         if (spotifyApi.getAccessToken()) {
-            fetchTop();
+            fetchData();
         }
     }, [spotifyApi, session]);
 
@@ -103,6 +127,7 @@ const UserContent = () => {
     return (
         <>  
             <div className='text-white w-full pb-64'>
+                HELLO BITCH {user_id} my email is {session?.user?.email} :D
                 {displayTopArtist(myTopArtists)}
                 {displayTopSongs(myTopSongs)}
             </div>
---
+++ src/pages/api/setStats.js
@@ -0,0 +1,65 @@
+import { sql } from '@vercel/postgres';
+import { v4 } from "uuid";
+
+export default async function getStats(req, res) {
+    const user_email = req.body.user_email;
+    const top_artists = req.body.top_artists;
+    const top_songs = req.body.top_songs;
+    let message = "";
+
+  try {
+    const createUsersTable = await sql`CREATE TABLE IF NOT EXISTS USERS (USER_ID varchar(200), EMAIL varchar(320), SHARE_STATS varchar(1), PRIMARY KEY(USER_ID, EMAIL));`;
+    const createTopArtistsTable = await sql`CREATE TABLE IF NOT EXISTS USER_TOP_ARTISTS (USER_ID varchar(200), ARTIST_RANKING int, ARTIST_ID varchar(200), STATS_DATE DATE);`;
+    const createTopSongsTable = await sql`CREATE TABLE IF NOT EXISTS USER_TOP_SONGS (USER_ID varchar(200), SONG_RANKING int, SONG_ID varchar(200), STATS_DATE DATE);`;
+
+    //console.log(`Created tables`);
+
+    const doesUserExist = await sql`SELECT USER_ID FROM USERS WHERE EMAIL = ${user_email}`;
+
+    //Create new ID for user if we aren't storing their account info
+    if(doesUserExist.rowCount < 1){
+
+        //console.log(`user does not exist: ${doesUserExist.user_id}, making id`);
+        const newUserId = v4();
+        const insertNewUser = await sql`INSERT INTO USERS (USER_ID, EMAIL, SHARE_STATS) VALUES (${newUserId}, ${user_email}, 'N') `
+    }
+
+    const user_id_query = await sql`SELECT USER_ID FROM USERS WHERE EMAIL = ${user_email}`;
+    const user_id = user_id_query.rows[0].user_id;
+    //console.log("User id for " + user_email + " is " + user_id)
+
+    const currentDate = getFormattedDate();
+
+    const isArtistsUpToDate = await sql`SELECT COUNT(*) FROM USER_TOP_ARTISTS WHERE USER_ID = ${user_id} and STATS_DATE = ${currentDate}`
+    if(isArtistsUpToDate.rows[0].count === '0'){
+      for(let i = 0; i < 5; i++){
+        const insertTopArtists = await sql`INSERT INTO USER_TOP_ARTISTS (USER_ID, ARTIST_RANKING, ARTIST_ID, STATS_DATE) VALUES (${user_id}, ${i}, ${top_artists?.[i]?.id}, ${currentDate})`;
+      }
+    }
+
+    const isSongsUpToDate = await sql`SELECT COUNT(*) FROM USER_TOP_SONGS WHERE USER_ID = ${user_id} and STATS_DATE = ${currentDate}`
+    if(isSongsUpToDate.rows[0].count === '0'){
+      for(let i = 0; i < 5; i++){
+        const insertTopSongs = await sql`INSERT INTO USER_TOP_SONGS (USER_ID, SONG_RANKING, SONG_ID, STATS_DATE) VALUES (${user_id}, ${i}, ${top_songs?.[i]?.id}, ${currentDate})`;
+      }
+    }
+
+    message = "Stats Saved";
+    } catch (error) {
+        message = "Error saving stats";
+        console.error(message, error);
+    }
+
+  return res.status(200).json({message});
+}
+
+function getFormattedDate() {
+  const currentDate = new Date();
+  const options = { day: 'numeric', month: 'short', year: 'numeric' };
+  const dateParts = currentDate.toLocaleDateString('en-GB', options).split(' ');
+
+  // Rearrange parts into 'dd-mmm-yyyy' format
+  const formattedDate = `${dateParts[0]}-${dateParts[1]}-${dateParts[2]}`;
+
+  return formattedDate;
+}
